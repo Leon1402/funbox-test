7766cafd10c81d8888ced6b1fc97947b5cca897b
Никита Каплюхин <Prog-2@muzgp12.ru>
2020-09-23 16:16:18 +0700

    Updates

---
+++ precache-manifest.b6df7f9a9b3116c6e04cb9a4f549160a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ddf9d3e7c25fddf0c539a1da60052a",
+    "url": "/funbox-test/index.html"
+  },
+  {
+    "revision": "2ad67510616a16dd6d2e",
+    "url": "/funbox-test/static/css/main.d6e2e526.chunk.css"
+  },
+  {
+    "revision": "05df0624819239842c7d",
+    "url": "/funbox-test/static/js/2.f37bc632.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funbox-test/static/js/2.f37bc632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad67510616a16dd6d2e",
+    "url": "/funbox-test/static/js/main.f05be756.chunk.js"
+  },
+  {
+    "revision": "ccedb79bf390273a5b0f",
+    "url": "/funbox-test/static/js/runtime-main.2a5fc341.js"
+  },
+  {
+    "revision": "68dc5d81d3c405a50111d6ded87a43ac",
+    "url": "/funbox-test/static/media/background.68dc5d81.png"
+  },
+  {
+    "revision": "0d5e09bdddcea3c081d175466a29c22f",
+    "url": "/funbox-test/static/media/cat.0d5e09bd.png"
+  }
+]);
