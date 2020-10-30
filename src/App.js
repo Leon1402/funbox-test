5862cbec713013241b3642dd6646c198e724aa96
Leon1402 <nikita-140294@mail.ru>
2020-10-30 13:08:20 +0700

    new

--- src/App.js
+++ src/App.js
@@ -9,16 +9,42 @@ const App = (props) => {
     elements.push(<ElementContainer key={i} index={i} />)
 
   return (
-    <div className='App'>
-      <div className='shadow-element-top'></div>
-      <div className='shadow-element-bottom'></div>
-      {/*upShadow, downShadow  элементы необходимые для прорисовки верхнейи нижней тени соответственно*/}
-      <div className='content'>
-        <header className='header'>
-          <h1>Ты сегодня покормил кота?</h1>
-        </header>
-        <div className='content-elements'>
-          {elements}
+    <div className='App' >
+      <div className="pattern">
+        <div className="shadow"></div>
+      </div>
+      <div className="App-body">
+        <div className="App-body__text">
+            Ты сегодня покормил кота?
+        </div>
+        <div className="elements">
+          <div className="elements__item elements__item_normal">
+              <div className="elements__item-">
+                
+              </div>
+            <div className="card">
+
+            </div>
+            <div className="sign-text">
+
+            </div>
+          </div>
+          <div className="elements__item elements__item_normal">
+            <div className="card">
+
+            </div>
+            <div className="sign-text">
+
+            </div>
+          </div>
+          <div className="elements__item elements__item_normal">
+            <div className="card">
+
+            </div>
+            <div className="sign-text">
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
